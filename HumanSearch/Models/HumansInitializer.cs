fcd928adbcc470b464414b1f20c2bd0b0a42b0ce
airburple <airburple@hotmail.com>
2016-09-03 22:45:02 -0600

    updated seeding initializer with image links and tweeked the home index view

--- HumanSearch/Models/HumansInitializer.cs
+++ HumanSearch/Models/HumansInitializer.cs
@@ -12,11 +12,11 @@ namespace HumanSearch.Models
         {
             var humans = new List<Human>
             {
-            new Human { Id= 1, Name = "Carson Alexandere", Address = "111 5th Street", Age = 19 ,FileName ="carsonalexander.jpg", Interests= "Volleyball camping hiking", Hair ="Brown" },
-            new Human { Id= 2, Name = "Ben Felix", Address = "222 6th BLVD", Age = 30 ,FileName ="benfelix.jpg", Interests= "Volleyball  hiking", Hair ="Bald" },
-            new Human { Id= 3, Name = "Susan Jones", Address = "333 11th Street", Age = 22 ,FileName ="susanjones.jpg", Interests= "Volleyball ", Hair ="Blonde" },
-            new Human { Id= 4, Name = "Ben Hope", Address = "444 14th Ave", Age = 65 ,FileName ="benhope.jpg", Interests= "camping hiking", Hair ="Brown" },
-            new Human { Id= 5, Name = "James Alexander", Address = "555 7th Street", Age = 40 ,FileName ="jamesalexander.jpg", Interests= "Racing", Hair ="Brown" },
+            new Human { Id= 1, Name = "Carson Alexandere", Address = "111 5th Street", Age = 19 ,FileName ="http://weknowyourdreams.com/images/smile/smile-02.jpg", Interests= "Volleyball camping hiking", Hair ="Brown" },
+            new Human { Id= 2, Name = "Ben Felix", Address = "222 6th BLVD", Age = 30 ,FileName ="http://weknowyourdreams.com/images/smile/smile-01.jpg", Interests= "Volleyball  hiking", Hair ="Bald" },
+            new Human { Id= 3, Name = "Susan Jones", Address = "333 11th Street", Age = 22 ,FileName ="http://weknowyourdreams.com/images/smile/smile-03.jpg", Interests= "Volleyball ", Hair ="Blonde" },
+            new Human { Id= 4, Name = "Ben Hope", Address = "444 14th Ave", Age = 65 ,FileName ="http://weknowyourdreams.com/images/smile/smile-04.jpg", Interests= "camping hiking", Hair ="Brown" },
+            new Human { Id= 5, Name = "James Alexander", Address = "555 7th Street", Age = 40 ,FileName ="http://weknowyourdreams.com/images/smile/smile-05.jpg", Interests= "Racing", Hair ="Brown" },
 
             };
 
